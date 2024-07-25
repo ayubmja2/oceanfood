7378e443ef2d7044784196f4e43259ef57f641b2
Ayub <ayubmja@icloud.com>
2024-07-25 10:49:40 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/echo.js',
+                'resources/js/echo-subscription.js',
+                'resources/js/post-form.js'
             ],
             refresh: true,
         }),
